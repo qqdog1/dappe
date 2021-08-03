c715a6e4bbec618d65e3aa92e613a0d85bf47fac
Shawn <qqdog1@yahoo.com.tw>
2021-08-03 22:11:27 +0800

    1. transfer token and save record

--- src/main/java/name/qd/dappe/config/ResponseEntityExceptionHandler.java
+++ src/main/java/name/qd/dappe/config/ResponseEntityExceptionHandler.java
@@ -1,13 +1,18 @@
 package name.qd.dappe.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ResponseEntityExceptionHandler {
+	private static Logger logger = LoggerFactory.getLogger(ResponseEntityExceptionHandler.class);
+	
 	@ExceptionHandler(value = Exception.class)
 	protected ResponseEntity<String> handleException(Exception e) {
+		logger.error(e.getMessage(), e);
 		return ResponseEntity.badRequest().body(e.toString());
 	}
 }
--- src/main/java/name/qd/dappe/service/WalletService.java
+++ src/main/java/name/qd/dappe/service/WalletService.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.web3j.abi.FunctionEncoder;
@@ -14,14 +16,18 @@ import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Function;
 import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.contracts.eip20.generated.ERC20;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.request.Transaction;
 import org.web3j.protocol.core.methods.response.EthCall;
+import org.web3j.protocol.core.methods.response.EthGasPrice;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.tx.Transfer;
+import org.web3j.tx.gas.DefaultGasProvider;
+import org.web3j.tx.gas.StaticGasProvider;
 import org.web3j.utils.Numeric;
 import org.web3j.utils.Convert.Unit;
 
@@ -32,6 +38,7 @@ import name.qd.dappe.repository.UserTransactionRepository;
 
 @Service
 public class WalletService {
+	private static Logger logger = LoggerFactory.getLogger(WalletService.class);
 	
 	@Autowired
 	private ConfigManager configManager;
@@ -83,10 +90,22 @@ public class WalletService {
 		return userTransaction;
 	}
 	
-	public UserTransaction transferToken(String currency, int id, String toAddress, BigDecimal amount) {
-		amount = amount.multiply(configManager.getContractDecimal(currency));
+	public UserTransaction transferToken(String currency, int id, String toAddress, BigDecimal amount) throws Exception {
+		UserAddress userAddress = addressService.getAddress(id);
+		if(userAddress == null) throw new Exception("id not exist.");
 		
-		return null;
+		Credentials credentials = Credentials.create(userAddress.getPkey());
+		BigDecimal decimalAmount = amount.multiply(configManager.getContractDecimal(currency));
+		BigInteger gasPrice = getLastGasPrice();
+		BigInteger gasLimit = BigInteger.valueOf(42000);
+		String contractAddress = configManager.getContractAddress(currency);
+		ERC20 erc20 = ERC20.load(contractAddress, web3j, credentials, new StaticGasProvider(gasPrice, gasLimit));
+		
+		TransactionReceipt transactionReceipt = erc20.transfer(toAddress, decimalAmount.toBigInteger()).send();
+		UserTransaction userTransaction = toUserTransaction(transactionReceipt, id, currency, amount);
+		
+		userTransaction = userTransactionRepository.save(userTransaction);
+		return userTransaction;
 	}
 	
 	private UserTransaction toUserTransaction(TransactionReceipt transactionReceipt, int id, String currency, BigDecimal amount) {
@@ -99,5 +118,15 @@ public class WalletService {
 		userTransaction.setToAddress(transactionReceipt.getTo());
 		userTransaction.setUserId(id);
 		return userTransaction;
+	}
+	
+	private BigInteger getLastGasPrice() {
+		try {
+			EthGasPrice ethGasPrice = web3j.ethGasPrice().send();
+			return ethGasPrice.getGasPrice();
+		} catch (IOException e) {
+			logger.error("Get last gas price failed.", e);
+		}
+		return DefaultGasProvider.GAS_LIMIT;
 	}
 }
